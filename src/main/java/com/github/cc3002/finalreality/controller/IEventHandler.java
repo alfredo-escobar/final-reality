c70fcb7939b8bdc555e2e420fc542947de0d757c
alfredo-escobar <alfredo.escobar@ug.uchile.cl>
2020-12-02 23:42:08 -0300

    Descripcion de IEventHandler

--- src/main/java/com/github/cc3002/finalreality/controller/IEventHandler.java
+++ src/main/java/com/github/cc3002/finalreality/controller/IEventHandler.java
@@ -2,5 +2,8 @@ package com.github.cc3002.finalreality.controller;
 
 import java.beans.PropertyChangeListener;
 
+/**
+ * Interface of the Handler class
+ */
 public interface IEventHandler extends PropertyChangeListener {
 }
